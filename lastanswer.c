46c136102cb492464822136e91644d68d66232b9
Syamir <syamirmdin@gmail.com>
2021-04-29 04:38:10 +0800

    Last Answer

--- lastanswer.c
+++ lastanswer.c
@@ -3,38 +3,40 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-
-
+	
 	void nameUser();
-
 	int main(void){
 	
-
 	for(int i = 1;i < 5; i++){
 	pid_t pid = fork();
-	if(pid == 0){
+	
+	
+
+		if(pid == 0){
 		char name[10];
-		printf("ENter name: ");
+		printf("Enter your name pls: ");
 		scanf("%s",name);
-		printf("Your name is %s\n",name);
-		return EXIT_SUCCESS;		
-	}
-	else if(pid != 0){
-		
-		wait(NULL);
-		
+		printf("Your name is %s.\n",name);
 		}
-	else {
+
+		else if(pid != 0){
+
+			wait(NULL);
+		return EXIT_SUCCESS;
+		}
+		else {
 		return EXIT_FAILURE;
 		}
 	}	
-	nameUser();
-	return EXIT_SUCCESS;
+
+		nameUser();
+		return EXIT_SUCCESS;
 
 	}
 	
 	void nameUser(){
+	
 		printf("Job is done.\n");
-	}
 
+	}
 
